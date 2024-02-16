16b24726f3fe4bea8b4be176930b030be4c0d204
ruah0807 <kimhk0315@gmail.com>
2024-02-16 12:08:06 +0900

    [feat] 플러스 기능 테스트
    세부사항들....

---
+++ src/main/java/org/ohgiraffers/practice2/calculator.java
@@ -0,0 +1,12 @@
+package org.ohgiraffers.practice2;
+
+import java.sql.PreparedStatement;
+
+public class calculator {
+
+    public int plusTwoNumbers (int firstNum, int secondNum) {
+        return firstNum+secondNum;
+    }
+
+
+}
